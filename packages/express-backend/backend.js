b0c7bd8c8410711471ce2159f28eef195ddbe1b6
nmateo1 <nmateo@calpoly.edu>
2024-04-17 21:27:32 -0700

    message

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,15 +4,47 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
+  app.get("/users", (req, res) => {
+    res.send(users);
+  });   
+
 const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
-  };
+  }; 
 
-app.use(express.json());
-
-app.get("/users", (req, res) => {
+  app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name != undefined) {
       let result = findUserByName(name);
@@ -21,7 +53,22 @@ app.get("/users", (req, res) => {
     } else {
       res.send(users);
     }
-  });
+  });  
+
+  const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);  
+
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });   
+
+app.use(express.json()); 
 
 app.listen(port, () => {
   console.log(
